72693ccbee5d7c2bd3cbb23f2cbb36aab96dc24a
kei-sasaki-rgb <kei5656@hotmail.com>
2019-11-18 20:53:36 +0900

    課題：不要になったアイテムを順次破棄しましょう
    Lesson6で作成したStepAsideUnityChanをもとに作成
    ユニティちゃんが通り過ぎて画面外に出たアイテムを直ちに破棄
    アイテムの破棄にはDestroy関数を使用

---
+++ Assets/DestroyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyController: MonoBehaviour {
+
+    //Unityちゃんのオブジェクト
+    private GameObject unitychan;
+    //Unityちゃんとカメラの距離
+    private float difference;
+
+    // Use this for initialization
+    void Start () {
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+        //Unityちゃんとカメラの位置（z座標)の差を求める
+        this.difference = unitychan.transform.position.z - this.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        //画面外に出たらオブジェクトを破棄する
+        if (this.transform.position.z < unitychan.transform.position.z)
+        Destroy(this.gameObject);
+    }
+}
